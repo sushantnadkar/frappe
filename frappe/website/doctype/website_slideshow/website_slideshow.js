e80b5f82174592e3b7428bf48569daab692e38f6
Sushant Nadkar <nadkarsushant@gmail.com>
2018-04-16 18:29:36 +0530

    parse youtube url and update image field for website slideshow item

--- frappe/website/doctype/website_slideshow/website_slideshow.js
+++ frappe/website/doctype/website_slideshow/website_slideshow.js
@@ -6,5 +6,13 @@ frappe.ui.form.on("Website Slideshow", {
 		let intro = frm.doc.__islocal?
 			"First set the name and save the record.": "Attach files / urls and add in table.";
 		frm.set_intro(intro);
+	},
+	validate: (frm) => {
+		for (var item = 0; item < frm.doc.slideshow_items.length; item++) {
+			if (frm.doc.slideshow_items[item].__unsaved == 1 && frm.doc.slideshow_items[item] == null) {
+				var  rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
+				frm.doc.slideshow_items[item].image = "https://img.youtube.com/vi/" + frm.doc.slideshow_items[item].video_link.match(rx)[1] + "/maxresdefault.jpg"
+			}
+		}
 	}
 })
